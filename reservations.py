b93d6d19a77af1dbfe157c213d6f7f853df4556e
SkriiL <flx.grimm@gmail.com>
2019-01-29 12:14:57 +0100

    #16 rechnungsadresse + staatsangehörigkeit auf reservierung

--- reservations.py
+++ reservations.py
@@ -10,7 +10,9 @@ def get_all():
     res_dir = []
     for r in reservations:
         res_dir.append({'name': r[0], 'dateFrom': r[1], 'dateTo': r[2], 'count': r[3], 'price': r[4], 'email': r[5], 'phone': r[6],
-                        'street': r[7], 'houseNumber': r[8], 'city': r[9], 'postalCode': r[10], 'country': r[11], 'id': r[12]})
+                        'street': r[7], 'houseNumber': r[8], 'city': r[9], 'postalCode': r[10], 'country': r[11], 'id': r[12],
+                        'nationality': r[13], 'isSameAsNormal': r[14], 'billStreet': r[15], 'billHouseNumber': r[16],
+                        'billCity': r[17], 'billPostalCode': r[18], 'billCountry': r[19]})
     return res_dir
 
 
@@ -18,8 +20,9 @@ def add(res_str, id=len(get_all()) + 1):
     res = res_str.split('|')
     conn = sqlite3.connect('db.db')
     c = conn.cursor()
-    params = (res[0], res[1], res[2], int(res[3]), res[4], res[5], res[6], res[7], res[8], res[9], int(res[10]), res[11], id)
-    c.execute('INSERT INTO reservations VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', params)
+    params = (res[0], res[1], res[2], int(res[3]), res[4], res[5], res[6], res[7], res[8], res[9], res[10], res[11],
+              id, res[13], res[14], res[15], res[16], res[17], res[18], res[19])
+    c.execute('INSERT INTO reservations VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', params)
     conn.commit()
     conn.close()
 

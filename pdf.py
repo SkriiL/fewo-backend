0a3975dc9b5f3a5831ac2bcb7aecca2ed9c2b1de
SkriiL <flx.grimm@gmail.com>
2019-01-29 17:28:31 +0100

    fix

--- pdf.py
+++ pdf.py
@@ -11,44 +11,44 @@ def create(res_id):
     c = canvas.Canvas('/var/www/html/assets/request.pdf')
 
     c.setFont('Courier', 10)
-    c.drawString(10, 825, "Meldeschein / Registration form")
+    c.drawString(20, 825, "Meldeschein / Registration form")
     c.drawString(260, 825, "Ferienwohnung Grimm | Mühlstraße 4 | 63579 Freigericht")
     form = c.acroForm
 
-    c.drawString(10, 790, "Datum Anreise")
+    c.drawString(20, 790, "Datum Anreise")
     form.textfield(name="dateFrom", tooltip="Datum Anreise", x=100, y=785, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=r.dateFrom, fontSize=14, height=20)
     c.drawString(300, 790, "Datum Abreise")
     form.textfield(name="dateTo", tooltip="Datum Abreise", x=390, y=785, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=r.dateTo, fontSize=14, height=20)
-    c.drawString(10, 760, "Name")
+    c.drawString(20, 760, "Name")
     form.textfield(name="name", tooltip="Name", x=100, y=755, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=440, textColor=black, forceBorder=True, value=r.name, fontSize=14, height=20)
-    c.drawString(10, 730, "Straße, Nr")
+    c.drawString(20, 730, "Straße, Nr")
     form.textfield(name="street", tooltip="Straße, Nr", x=100, y=725, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=440, textColor=black, forceBorder=True, value=r.street + ' ' + r.houseNumber,
                    fontSize=14, height=20)
-    c.drawString(10, 700, "PLZ, Ort")
+    c.drawString(20, 700, "PLZ, Ort")
     form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=240, textColor=black, forceBorder=True, value=r.postalCode + ', ' + r.city,
                    fontSize=14, height=20)
     c.drawString(353, 700, "Land")
     form.textfield(name="country", tooltip="Land", x=390, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=r.country, fontSize=14, height=20)
-    c.drawString(10, 670, "Passnummer")
+    c.drawString(20, 670, "Passnummer")
     form.textfield(name="passport", tooltip="Passnummer", x=100, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
     c.drawString(270, 670, "Staatsangehörigkeit")
     form.textfield(name="nationality", tooltip="Staatsangehörigkeit", x=390, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=r.nationality.lower(), fontSize=14, height=20)
-    c.drawString(10, 640, "Geburtsdatum")
+    c.drawString(20, 640, "Geburtsdatum")
     form.textfield(name="birthdate", tooltip="Geburtsdatum", x=100, y=635, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=190, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
     c.drawString(311, 640, "Mitreisende")
     form.textfield(name="count", tooltip="Anzahl der Mitreisenden", x=390, y=635, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=str(r.count), fontSize=14, height=20)
 
-    c.drawString(10, 590, "Unterschrift")
+    c.drawString(20, 590, "Unterschrift")
     form.textfield(name="signature", tooltip="Unterschrift", x=100, y=585, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=440, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
 
@@ -56,14 +56,14 @@ def create(res_id):
                   borderColor=black, fillColor=white, textColor=black, forceBorder=True)
     c.drawString(40, 540, "Die Rechnungsanschrift stimmt mit der Privatanschrift überein")
 
-    c.drawString(10, 510, "Firmenname")
+    c.drawString(20, 510, "Firmenname")
     form.textfield(name="companyName", tooltip="Firmenname", x=100, y=505, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=440, textColor=black, forceBorder=True, value=r.companyName, fontSize=14, height=20)
-    c.drawString(10, 480, "Straße, Nr")
+    c.drawString(20, 480, "Straße, Nr")
     form.textfield(name="street", tooltip="Straße, Nr", x=100, y=475, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=440, textColor=black, forceBorder=True, value=r.billStreet + " " + r.billHouseNumber,
                    fontSize=14, height=20)
-    c.drawString(10, 450, "PLZ, Ort")
+    c.drawString(20, 450, "PLZ, Ort")
     form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=445, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=240, textColor=black, forceBorder=True, value=r.billPostalCode + ", " + r.billCity,
                    fontSize=14, height=20)

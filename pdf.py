66feba4494d21dd87ad911d6a58e8ff879fed3c0
SkriiL <flx.grimm@gmail.com>
2019-02-02 00:17:04 +0100

    #21 invoice pdf

--- pdf.py
+++ pdf.py
@@ -27,10 +27,10 @@ def create_registration_form(res_id):
                    fillColor=white, width=150, textColor=black, forceBorder=True, value=r.dateTo, fontSize=14, height=20)
     c.drawString(20, 760, "Name")
     form.textfield(name="name", tooltip="Name", x=100, y=755, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=r.name, fontSize=14, height=20)
+                   fillColor=white, width=440, textColor=black, forceBorder=True, value=str(r.name, "utf-8"), fontSize=14, height=20)
     c.drawString(20, 730, "Straße, Nr")
     form.textfield(name="street", tooltip="Straße, Nr", x=100, y=725, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=r.street + ' ' + r.houseNumber,
+                   fillColor=white, width=440, textColor=black, forceBorder=True, value=str(r.street, "utf-8") + ' ' + r.houseNumber,
                    fontSize=14, height=20)
     c.drawString(20, 700, "PLZ, Ort")
     form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
@@ -38,13 +38,13 @@ def create_registration_form(res_id):
                    fontSize=14, height=20)
     c.drawString(353, 700, "Land")
     form.textfield(name="country", tooltip="Land", x=390, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=r.country, fontSize=14, height=20)
+                   fillColor=white, width=150, textColor=black, forceBorder=True, value=str(r.country, "utf-8"), fontSize=14, height=20)
     c.drawString(20, 670, "Passnummer")
     form.textfield(name="passport", tooltip="Passnummer", x=100, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=150, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
     c.drawString(270, 670, "Staatsangehörigkeit")
     form.textfield(name="nationality", tooltip="Staatsangehörigkeit", x=390, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=r.nationality.lower(), fontSize=14, height=20)
+                   fillColor=white, width=150, textColor=black, forceBorder=True, value=str(r.nationality.lower(), "utf-8"), fontSize=14, height=20)
     c.drawString(20, 640, "Geburtsdatum")
     form.textfield(name="birthdate", tooltip="Geburtsdatum", x=100, y=635, borderStyle="inset", borderColor=black, fontName="Courier",
                    fillColor=white, width=190, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
@@ -62,18 +62,18 @@ def create_registration_form(res_id):
 
     c.drawString(20, 510, "Firmenname")
     form.textfield(name="companyName", tooltip="Firmenname", x=100, y=505, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=r.companyName, fontSize=14, height=20)
+                   fillColor=white, width=440, textColor=black, forceBorder=True, value=str(r.companyName, "utf-8"), fontSize=14, height=20)
     c.drawString(20, 480, "Straße, Nr")
     form.textfield(name="street", tooltip="Straße, Nr", x=100, y=475, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=r.billStreet + " " + r.billHouseNumber,
+                   fillColor=white, width=440, textColor=black, forceBorder=True, value=str(r.billStreet, "utf-8") + " " + r.billHouseNumber,
                    fontSize=14, height=20)
     c.drawString(20, 450, "PLZ, Ort")
     form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=445, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=240, textColor=black, forceBorder=True, value=r.billPostalCode + ", " + r.billCity,
+                   fillColor=white, width=240, textColor=black, forceBorder=True, value=r.billPostalCode + ", " + str(r.billCity, "utf-8"),
                    fontSize=14, height=20)
     c.drawString(353, 450, "Land")
     form.textfield(name="country", tooltip="Land", x=390, y=445, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=r.billCountry, fontSize=14, height=20)
+                   fillColor=white, width=150, textColor=black, forceBorder=True, value=str(r.billCountry, "utf-8"), fontSize=14, height=20)
     c.save()
 
 

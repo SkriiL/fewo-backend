453713ab703a306c672245f5ddd87526b2a4b5dc
SkriiL <flx.grimm@gmail.com>
2019-02-02 00:28:10 +0100

    #21 invoice pdf

--- pdf.py
+++ pdf.py
@@ -13,8 +13,8 @@ def make_nice(text):
     text = text.replace("ä", "ae")
     text = text.replace("Ö", "Oe")
     text = text.replace("ö", "oe")
-    text = text.replace("Ü", "Üe")
-    text = text.replace("ü", "üe")
+    text = text.replace("Ü", "Ue")
+    text = text.replace("ü", "ue")
     text = text.replace("ß", "ss")
     return text
 

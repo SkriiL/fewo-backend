a61329a9475835d34e998553e1961a4c960a28e0
SkriiL <flx.grimm@gmail.com>
2019-02-02 14:25:09 +0100

    #24 style registration form

--- pdf.py
+++ pdf.py
@@ -25,66 +25,23 @@ def create_registration_form(res_id):
     r.values_to_model(res)
     c = canvas.Canvas('/var/www/html/assets/requestForm.pdf')
 
-    c.setFont('Courier', 10)
-    c.drawString(20, 815, "Meldeschein / Registration form")
-    c.drawString(260, 815, "Ferienwohnung Grimm | Mühlstraße 4 | 63579 Freigericht")
+    pdfmetrics.registerFont(TTFont('Calibri', "Calibri.ttf"))
+    pdfmetrics.registerFont(TTFont('CalibriBold', "CALIBRIB.TTF"))
+
+    c.setFont('CalibriBold', 11)
+    c.drawString(40, 815, "Meldeschein / Registration form")
+    c.setFont("Calibri", 11)
+    c.drawRightString(555, 815, "Ferienwohnung Grimm | Mühlstraße 4 | 63579 Freigericht")
     form = c.acroForm
 
-    c.drawString(20, 790, "Datum Anreise")
-    form.textfield(name="dateFrom", tooltip="Datum Anreise", x=100, y=785, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=r.dateFrom, fontSize=14, height=20)
-    c.drawString(300, 790, "Datum Abreise")
-    form.textfield(name="dateTo", tooltip="Datum Abreise", x=390, y=785, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=r.dateTo, fontSize=14, height=20)
-    c.drawString(20, 760, "Name")
-    form.textfield(name="name", tooltip="Name", x=100, y=755, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=make_nice(r.name), fontSize=14, height=20)
-    c.drawString(20, 730, "Straße, Nr")
-    form.textfield(name="street", tooltip="Straße, Nr", x=100, y=725, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=make_nice(r.street) + ' ' + r.houseNumber,
-                   fontSize=14, height=20)
-    c.drawString(20, 700, "PLZ, Ort")
-    form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=240, textColor=black, forceBorder=True, value=r.postalCode + ', ' + make_nice(r.city),
-                   fontSize=14, height=20)
-    c.drawString(353, 700, "Land")
-    form.textfield(name="country", tooltip="Land", x=390, y=695, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=make_nice(r.country), fontSize=14, height=20)
-    c.drawString(20, 670, "Passnummer")
-    form.textfield(name="passport", tooltip="Passnummer", x=100, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
-    c.drawString(270, 670, "Staatsangehörigkeit")
-    form.textfield(name="nationality", tooltip="Staatsangehörigkeit", x=390, y=665, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=make_nice(r.nationality.lower()), fontSize=14, height=20)
-    c.drawString(20, 640, "Geburtsdatum")
-    form.textfield(name="birthdate", tooltip="Geburtsdatum", x=100, y=635, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=190, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
-    c.drawString(311, 640, "Mitreisende")
-    form.textfield(name="count", tooltip="Anzahl der Mitreisenden", x=390, y=635, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=str(r.count), fontSize=14, height=20)
+    c.line(40, 800, 555, 800)
 
-    c.drawString(20, 590, "Unterschrift")
-    form.textfield(name="signature", tooltip="Unterschrift", x=100, y=585, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value="", fontSize=14, height=20)
-
-    form.checkbox(name="bill", tooltip="Rechnungsanschrift stimmt überein", x=20, y=535, borderStyle="inset",
-                  borderColor=black, fillColor=white, textColor=black, forceBorder=True, checked=r.isSameAsNormal)
-    c.drawString(50, 540, "Die Rechnungsanschrift stimmt mit der Privatanschrift überein")
-
-    c.drawString(20, 510, "Firmenname")
-    form.textfield(name="companyName", tooltip="Firmenname", x=100, y=505, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=make_nice(r.companyName), fontSize=14, height=20)
-    c.drawString(20, 480, "Straße, Nr")
-    form.textfield(name="street", tooltip="Straße, Nr", x=100, y=475, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=440, textColor=black, forceBorder=True, value=make_nice(r.billStreet) + " " + r.billHouseNumber,
-                   fontSize=14, height=20)
-    c.drawString(20, 450, "PLZ, Ort")
-    form.textfield(name="city", tooltip="PLZ, Ort", x=100, y=445, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=240, textColor=black, forceBorder=True, value=r.billPostalCode + " " + make_nice(r.billCity),
-                   fontSize=14, height=20)
-    c.drawString(353, 450, "Land")
-    form.textfield(name="country", tooltip="Land", x=390, y=445, borderStyle="inset", borderColor=black, fontName="Courier",
-                   fillColor=white, width=150, textColor=black, forceBorder=True, value=make_nice(r.billCountry), fontSize=14, height=20)
+    c.setFont('CalibriBold', 11)
+    c.drawString(50, 790, "Privatanschrift / Home address")
+    c.drawString(200, 790, "Datum Anreise /")
+    c.drawString(200, 780, "Date of arrival")
+    c.drawString(400, 790, "Datum Abreise /")
+    c.drawString(400, 780, "Date of departure")
     c.save()
 
 
